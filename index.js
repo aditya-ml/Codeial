7cfbfd63039cec3e2da9b32bea12284d7e5094e6
unknown <aditya98singhal@gmail.com>
2021-04-16 14:10:45 +0530

    Add a controller and router

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const router = require('./routes');
 const app = express();
 const port = 8000;
 
+//use express router
+app.use('/', require('./routes'))
+
+//access home_controller here
+const homeController = require('./controllers/home_controller');
+router.get('/', homeController.home) // '/' is the url where I want to put it
 
 app.listen(port, function(err){
     if(err){
---
+++ routes/index.js
@@ -0,0 +1,8 @@
+//Entry point to all routes
+
+const express = require("express");
+
+const router = express.Router();
+
+
+module.exports = router; //tell app to use it
