13f10459195a42966c1cbbb493e0b251836da6e6
unknown <aditya98singhal@gmail.com>
2021-04-22 19:23:40 +0530

    Changed multiple files for Passport.js

--- routes/users.js
+++ routes/users.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const router = express.Router();
 
+const passport = require('../config/passport-local-strategy');
+
 const usersController = require('../controllers/users_controller');
 
-router.get('/profile',usersController.profile);
+router.get('/profile', passport.checkAuthentication, usersController.profile);
 
 router.get('/sign-up', usersController.signUp);
 router.get('/sign-in', usersController.signIn);
 
 router.post('/create', usersController.create);
 
+// use passport as a middleware to authemticate
+router.post('/create-session', 
+    passport.authenticate('local',{failureRedirect: '/users/sign-in'}
+    ), 
+    usersController.createSession);
+
 module.exports = router;
+
+router.get('/sign-out', usersController.destroySession);
