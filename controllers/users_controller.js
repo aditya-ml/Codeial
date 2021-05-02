754fd716b9c1dc9895ad8b1f501888563d194f19
unknown <aditya98singhal@gmail.com>
2021-05-02 12:03:26 +0530

    Code for delete and update objects in db, distribute views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -3,10 +3,24 @@ const User = require('../models/user');
 
 module.exports.profile = function(req, res){
     // res.end('<h1>User Profile</h1>');
-    return res.render('users', {
-        //context
-        title: "Users"
-    })
+    User.findById(req.params.id, function(err, user){
+        return res.render('users', {
+            //context
+            title: "User Profile",
+            profile_user: user
+        });
+    });
+    
+}
+
+module.exports.update = function(req, res){
+    if(req.user.id == req.params.id){
+        User.findByIdAndUpdate(req.params.id, req.body, function(err, user){
+            return res.redirect('back');
+        });
+    }else{
+        return res.status(401).send('Unauthorized');
+    }
 }
 
 // render the sign up page
--- routes/comments.js
+++ routes/comments.js
@@ -7,5 +7,6 @@ const passport = require('../config/passport-local-strategy');
 const commentsController = require('../controllers/comments_controller');
 
 router.post('/create', passport.checkAuthentication, commentsController.create);
+router.get('/destroy/:id', passport.checkAuthentication, commentsController.destroy);
 
 module.exports = router;
